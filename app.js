383330c162a820a96ca935f5bb8c77bc841f6531
Anurag Sisodiya <anurps9@gmail.com>
2021-06-27 23:14:31 +0530

    debug

--- app.js
+++ app.js
@@ -275,7 +275,7 @@ app.post('/mailScreen', (req, res) => {
 				var cronStringInterval; 
 				if(interval.localeCompare('every-minute')==0){
 					task.mailInfo.recurrence = 'Every minute';
-					cronStringInterval = '* * * * * *';
+					cronStringInterval = '* * * * *';
 				}else if(interval.localeCompare('every-week')==0){
 					task.mailInfo.recurrence = 'Every week';
 					cronStringInterval = '* * * * 1';
